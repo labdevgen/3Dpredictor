3c10ae330da48000bedca493fcef06e10725b12a
labdevgen <zakazserov@gmail.com>
2018-08-16 20:02:56 +0700

    Added Contacts reader

--- Contacts_reader.py
+++ Contacts_reader.py
@@ -1,5 +1,52 @@
-import logging
-from shared import FileReader
+import logging, os
+import pandas as pd
+import numpy as np
+from shared import Interval
 
-class ContactsReader(FileReader):
-    pass
+MAX_CHR_DIST = 3000000000
+
+class ContactsReader():
+    def __init__(self):
+        self.data = {}
+        self.binsize = -1
+
+    def read_file(self,chr,fname):
+        logging.info("Reading file "+fname)
+        if chr in self.data:
+            logging.warning("Chromosome "+chr+" will be rewritten")
+
+        data = pd.read_csv(fname, delimiter="\t", names=["st", "en", "count"])
+        data["chr"] = [chr] * len(data)
+        data["dist"] = data["en"] - data["st"]
+        assert np.all(data["dist"]) >= 0
+        binsize = min(data["dist"][data["dist"] > 0])
+        if self.binsize != -1 and binsize != self.binsize:
+            logging.error("Binsize in file "+str(fname)
+                          +"("+binsize+") does not match binsize "
+                          +str(self.binsize))
+        elif self.binsize == -1:
+            self.binsize = binsize
+            logging.info("Bin Size set to "+str(binsize))
+
+        #data.sort_values(by=["st","en"],inplace=True)
+        self.data[chr] = data
+
+    def read_files(self,fnames):
+        for f in fnames:
+            self.read_file(os.path.basename(f).split(".")[0],f)
+
+    def get_contacts(self,interval,maxdist=MAX_CHR_DIST):
+        logging.debug(self.data[interval.chr].head())
+        return self.data[interval.chr].query(
+              "@interval.start < st < @interval.end & "
+            + "@interval.start < en < @interval.end & "
+            + "dist <=@maxdist")
+
+    def get_min_contact_position(self,chr):
+        return min(self.data[chr]["st"].values)
+
+    def get_max_contact_position(self,chr):
+        return max(self.data[chr]["en"].values)
+
+    def get_chrms(self):
+        return self.data.keys()
--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
-from ChiPSeqReader import ChiPSeqReader
-from shared import Interval
 import logging
+from ChiPSeqReader import ChiPSeqReader
+from Contacts_reader import ContactsReader
+from shared import Interval
+
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -11,3 +13,11 @@ def test_ctcf(): #comment
     logging.info(d)
     d1 = ctcf_reader.get_binned_interval(Interval("chr1",3448200,3457000),binsize=500)
     logging.info(d1)
+
+def test_contacts():
+    contacts_reader = ContactsReader()
+    contacts_reader.read_files(["C:/Users/FishmanVS/Desktop/RNF3D_beds/chr1.Hepat.contacts"])
+    c = contacts_reader.get_contacts(Interval("chr1",5000000,6000000))
+    logging.info(c)
+
+test_contacts()
