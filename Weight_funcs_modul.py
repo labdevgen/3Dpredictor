ab8d136b99fbbe2f9e66b323fb868dd166a09ccd
BelokopytovaPS <belka2195@mail.ru>
2018-09-28 16:10:19 +0700

    overweight_loops

--- Weight_funcs_modul.py
+++ Weight_funcs_modul.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import numpy as np
 from functools import partial
 
@@ -19,29 +18,16 @@ def decorate_mult_abs_log(func,coeff):
     result.__name__ = str(coeff) + func.__name__
     return result
 
-def overweight_loops(contacts,predictors):
-    pass
-=======
-import numpy as np
-from functools import partial
-
-def ones_like(contacts, *args):
-    return np.ones_like(contacts)
-
-def array(contacts,*args):
-    return np.array(contacts)
-
-def abs_log(contacts,predictors):
-    return np.abs(np.log(contacts))
-
-def mult_abs_log(contacts,predictors,coeff):
-    return np.abs(np.log(contacts))*coeff
-
-def decorate_mult_abs_log(func,coeff):
+def overweight_loops(contacts,predictors,coeff): #contacts is np.array of contact_count, predictors
+    result = np.ones((len(contacts),), dtype=int)
+    idx = predictors.columns.get_loc("isloop")
+    for i in range(len(contacts)):
+        if predictors.iloc[i, idx] == 1:
+            result[i] = 
+def decorate_overweight_loops(func,coeff):
     result = partial(func,coeff=coeff)
     result.__name__ = str(coeff) + func.__name__
     return result
 
-def overweight_loops(contacts,predictors):
-    pass
->>>>>>> origin/master
+
+
